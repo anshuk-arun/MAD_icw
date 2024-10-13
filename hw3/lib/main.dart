8baf1b1b15535557e2b0854ca22db8592bce642d
anshuk-arun <64498261+anshuk-arun@users.noreply.github.com>
2024-10-13 12:02:11 -0400

    stateful hw3

--- hw3/lib/main.dart
+++ hw3/lib/main.dart
@@ -29,58 +29,117 @@ class MatchGameScreen extends StatefulWidget {
   State<MatchGameScreen> createState() => _MatchGameScreenState();
 }
 
-class Card {
+class Card extends StatefulWidget{
+  final Widget? child;
+
+  Card ({
+    super.key,
+    this.child,
+  });
+
+  // Card (String cardText{
+  //   super.key,
+  
+  //   this.child,
+  //   // _backDesign;
+  //   // _cardText = cardText;
+  //   // _isFaceUp = false;
+  // });
+  
+  @override
+  State<Card> createState() => _CardState();
+
+}
+
+class _CardState extends State<Card> {
+
   final Image _backDesign = Image.asset("purple-card-back.jpg");
   late String _cardText;
   late bool _isFaceUp;
 
-  Card (String cardText){
-    _backDesign;
-    _cardText = cardText;
+  void createCard(String ct){
+    _cardText = ct;
     _isFaceUp = false;
   }
 
-}
-class _MatchGameScreenState extends State<MatchGameScreen> {
- 
-  List<Card> cards = [
-    Card("Hello"),
-    Card("World"),
-  ];
-
-
-  void flipCard(int index){
-    setState(() {
-      cards[index]._isFaceUp = !cards[index]._isFaceUp;
-    });
+  void flipCard(){
+    _isFaceUp = !_isFaceUp;
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/purple-card-back.jpg'),
+            fit: BoxFit.contain,
+          ),
+          shape: BoxShape.rectangle,
+        ),
+      child: GestureDetector(
+        onTap: flipCard,
+      ),
+    );
+  }
+
+}
+
+class _MatchGameScreenState extends State<MatchGameScreen> {
+  
+  List<Card> cards = [];
+  // cards[0] = Card.
+  // List<Card> cards = [
+  //   Card("Hello"),
+  //   Card("World"),
+  // ];
+
+  
+
+
+  // void flipCard(int index){
+  //   setState(() {
+  //     cards[index]._isFaceUp = !cards[index]._isFaceUp;
+  //   });
+  // }
 
   // do cards match?
   // delete from cards list
-  void deleteCard(int index){
-    setState(() {
-      cards.removeAt(index);
-    });
-  }
+  // void deleteCard(int index){
+  //   setState(() {
+  //     cards.removeAt(index);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: Colors.white,
       body: Center(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-            itemCount: cards.length,
-            itemBuilder: (context, index){
-              return GridTile(
-                child: Text(cards[index]._cardText),
-              );
-            },
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
 
-      ),
+        ),
+      )
+
     );
+    // return Scaffold(
+    //   backgroundColor: Colors.lightBlueAccent,
+    //   body: Center(
+    //       child: GridView.builder(
+    //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+    //         itemCount: cards.length,
+    //         itemBuilder: (context, index){
+    //           return GridTile(
+    //             child: Text(cards[index]._cardText),
+    //           );
+    //         },
+    //     ),
+
+    //   ),
+    // );
 
   }
 }
